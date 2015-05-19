194f8739cfbba5e43dcf146cfd69935cbdcac83b
justind000 <Justind000@gmail.com>
2015-05-18 18:56:41 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,22 @@
 #include "wiring.h"
-#include "pwr_clk_mgmt.h"
+#include "nrf.h"
 
 #define DEBUG 1
 
-void main()
-{
+void setup(){
 	serialBegin();
-	wireBegin();
-	analogSetup();
 
 	debugPrint("i/o node starting - sender", 0);
-	watchdogRun(1000)
-
+	watchdogRun(1000);
+	
 	pinMode(GPIO_PIN_ID_FUNC_AIN0, INPUT | GPIO_PIN_CONFIG_OPTION_PIN_MODE_INPUT_BUFFER_ON_PULL_DOWN_RESISTOR);
 	if(!digitalRead(GPIO_PIN_ID_FUNC_AIN0)) 
 	{
 		//second lowest power mode
-		pwr_clk_mgmt_enter_pwr_mode_memory_ret_tmr_off();
+		sleep(PWR_CLK_MGMT_PWRDWN_MODE_MEMORY_RET_TMR_OFF);
 	}
-	else
-	{
-		//spin wheels while waiting to wd reset
-		while(1){}
-	}
+}
+
+void loop(){
+
 }
