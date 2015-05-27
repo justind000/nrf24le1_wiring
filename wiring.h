f12d6cd75e437d782fecd25b8d4db47ea3cba3ac
justind000 <Justind000@gmail.com>
2015-05-26 20:10:15 -0700

    Update wiring.h

--- wiring.h
+++ wiring.h
@@ -20,6 +20,8 @@
 #define pinMode gpio_pin_configure
 #define OUTPUT GPIO_PIN_CONFIG_OPTION_DIR_OUTPUT
 #define INPUT GPIO_PIN_CONFIG_OPTION_DIR_INPUT
+#define HIGH_POWER GPIO_PIN_CONFIG_OPTION_PIN_MODE_OUTPUT_BUFFER_HIGH_DRIVE_STRENGTH
+#define NORMAL_POWER GPIO_PIN_CONFIG_OPTION_PIN_MODE_OUTPUT_BUFFER_NORMAL_DRIVE_STRENGTH
 #define LOW 0
 #define HIGH 1
 #define delay delay_ms
@@ -44,6 +46,9 @@
 #define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
 #define bit(b) (1UL << (b))
 
+#define PULLDOWN GPIO_PIN_CONFIG_OPTION_PIN_MODE_INPUT_BUFFER_ON_PULL_DOWN_RESISTOR
+#define PULLUP GPIO_PIN_CONFIG_OPTION_PIN_MODE_INPUT_BUFFER_ON_PULL_UP_RESISTOR
+
 //PWR_CLK_MGMT_PWRDWN_MODE_ACTIVE, PWR_CLK_MGMT_PWRDWN_MODE_STANDBY, PWR_CLK_MGMT_PWRDWN_MODE_REGISTER_RET
 //PWR_CLK_MGMT_PWRDWN_MODE_MEMORY_RET_TMR_ON, PWR_CLK_MGMT_PWRDWN_MODE_MEMORY_RET_TMR_OFF, PWR_CLK_MGMT_PWRDWN_MODE_DEEP_SLEEP
 #define sleep(mode) PWRDWN = ((PWRDWN & ~(PWRDWN_PWR_CNTL_MASK)) | mode) 
@@ -51,6 +56,39 @@
 #ifdef DEBUG
 	#undef DEBUG
 #endif
+
+
+#define P0_0 GPIO_PIN_ID_P0_0
+#define P0_1 GPIO_PIN_ID_P0_1
+#define P0_2 GPIO_PIN_ID_P0_2
+#define P0_3 GPIO_PIN_ID_P0_3
+#define P0_4 GPIO_PIN_ID_P0_4
+#define P0_5 GPIO_PIN_ID_P0_5
+#define P0_6 GPIO_PIN_ID_P0_6
+#define P0_7 GPIO_PIN_ID_P0_7
+#define P1_0 GPIO_PIN_ID_P1_0
+#define P1_1 GPIO_PIN_ID_P1_1
+#define P1_2 GPIO_PIN_ID_P1_2
+#define P1_3 GPIO_PIN_ID_P1_3
+#define P1_4 GPIO_PIN_ID_P1_4
+#define P1_5 GPIO_PIN_ID_P1_5
+#define P1_6 GPIO_PIN_ID_P1_6
+#define P1_7 GPIO_PIN_ID_P1_7
+#define P2_0 GPIO_PIN_ID_P2_0
+#define P2_1 GPIO_PIN_ID_P2_1
+#define P2_2 GPIO_PIN_ID_P2_2
+#define P2_3 GPIO_PIN_ID_P2_3
+#define P2_4 GPIO_PIN_ID_P2_4
+#define P2_5 GPIO_PIN_ID_P2_5
+#define P2_6 GPIO_PIN_ID_P2_6
+#define P2_7 GPIO_PIN_ID_P2_7
+#define P3_0 GPIO_PIN_ID_P3_0
+#define P3_1 GPIO_PIN_ID_P3_1
+#define P3_2 GPIO_PIN_ID_P3_2
+#define P3_3 GPIO_PIN_ID_P3_3
+#define P3_4 GPIO_PIN_ID_P3_4
+#define P3_5 GPIO_PIN_ID_P3_5
+#define P3_6 GPIO_PIN_ID_P3_6
 
 typedef unsigned int word;
 typedef uint8_t boolean;
@@ -62,12 +100,16 @@ void putchar(char c);
 char getchar();
 void serialBegin();
 void wireBegin();
+uint8_t eepromRead(uint16_t address);
+void eepromWrite(uint16_t address, uint8_t value);
 w2_ack_nack_val_t wireWrite8(uint8_t slave_address, uint8_t data);
 uint8_t wireRead8(uint8_t slave_address, uint8_t address);
 uint16_t wireRead16(uint8_t slave_address, uint8_t address);
 void gpioSetup();
 #define watchdogRun(p1) watchdog_start_and_set_timeout_in_ms(p1);CLKLFCTRL=1
 interrupt_isr_rfirq();
+unsigned int i = 0;
+unsigned char control_byte, address_byte, data_byte;
 
 void main(){
 	wireBegin();
@@ -122,7 +164,7 @@ char getchar()
 	return retchar;
 }
 
-w2_ack_nack_val_t wireWrite8(uint8_t slave_address, uint8_t ww8data){
+w2_ack_nack_val_t wireWrite8	(uint8_t slave_address, uint8_t ww8data){
 	
 	if(w2_master_write_to(slave_address, &ww8data, 1, 0, 0) ==  W2_NACK_VAL)
 	{
@@ -151,6 +193,31 @@ uint16_t wireRead16(uint8_t slave_address, uint8_t address){
 		return W2_NACK_VAL;}
 	else{
 		return wr16data;
+	}
+}
+
+uint8_t eepromRead(uint16_t address){
+		control_byte = (unsigned char)(0x50 | (address >> 8));
+		address_byte = (unsigned char)address;
+
+		if(w2_master_random_address_read(control_byte, &address_byte, 1, &data_byte, 1) ==  W2_NACK_VAL)
+		{
+			//printf_tiny("NACK on address 0x%x", i);
+			return -1;
+		}
+	return data_byte;	
+
+}
+
+void eepromWrite(uint16_t address, uint8_t value){
+	control_byte = (unsigned char)(0x50 | (address >> 8));
+	address_byte = (unsigned char)address;
+	data_byte = (unsigned char)value;
+
+	if(w2_master_write_to(control_byte, &address_byte, 1, &data_byte, 1) ==  W2_NACK_VAL)
+	{
+		//printf_tiny("NACK on address 0x%x", i);
+		return;
 	}
 }
 #endif
