4d8f422467f8d4201b144463cb5b78f8a5f07354
Jens Jensen <af5mi@yahoo.com>
2017-02-12 09:26:22 -0600

    numerous changes
    * moving more dependencies back to core nrf24le1 sdk
    * playing with auto-ack and retires to improve resiliency
    * setting up watchdog timer
    * setup and use 1 sec tick timer (rtc)
    * use low power sleep mode

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "wiring.h"
 #include "rf.h"
 #include <stdio.h>
+#include "rtc2.h"
+
 #define printf	printf_fast_f
 
 #define DEBUG 1
-
+#define WATCHDOG_TIME		0xA00	// about 20 seconds. WDSV*256/32768 = wdt reset period
 #define LED2	P0_1
 #define LED3	P0_2
 #define LED_ON	LOW
@@ -12,15 +14,17 @@
 #define SW_K2	P1_6
 #define SW_K3	P1_5
 
-uint16_t counter = 1001;
-uint8_t txaddr[] = {'J','E','N','S', 0x01}; // backwards on rpi
-///uint8_t txaddr[] = {0x01, 'S', 'N', 'E', 'J'};
+uint16_t counter = 0x0D;
+//uint8_t txaddr[] = {'J','E','N','S', 0x01}; // backwards on rpi
+uint8_t txaddr[] = {0x02, 'S', 'N', 'E', 'J'};
 
 uint8_t regbuf[2];
 uint16_t adcraw;
+__idata volatile uint32_t seconds = 0;
 
 enum msgtype {
-  MSG_POWER_METER
+  MSG_POWER_METER,
+  MSG_COUNT_VOLTS
 };
 
 typedef struct {
@@ -32,25 +36,59 @@ typedef struct {
   uint32_t counter;
 } msg_power_meter;
 
-msg_power_meter msg;
+typedef struct {
+	uint8_t msgtype;
+	uint32_t counter;
+	float voltage;
+} msg_count_volts;
+
+//msg_power_meter msg;
+msg_count_volts msg;
+
+interrupt_isr_rtc2()
+{
+	seconds++;
+}
 
 float raw2volts(uint16_t raw) {
-	#define VREF1_2  1.175
+	#define VREF1_2  1.175		// nominal 1.2V, chosen to get accurate voltage reading
 	float voltage = raw * VREF1_2 * 3.0 / 1024.0  ;
 	return voltage;
 }
 
+void uart_init(){
+
+	//Set up UART pin
+	gpio_pin_configure(GPIO_PIN_ID_FUNC_TXD,
+					   GPIO_PIN_CONFIG_OPTION_DIR_OUTPUT |
+					   GPIO_PIN_CONFIG_OPTION_OUTPUT_VAL_SET |
+					   GPIO_PIN_CONFIG_OPTION_PIN_MODE_OUTPUT_BUFFER_NORMAL_DRIVE_STRENGTH);
+
+	gpio_pin_configure(GPIO_PIN_ID_FUNC_RXD,
+					   GPIO_PIN_CONFIG_OPTION_DIR_INPUT |
+					   GPIO_PIN_CONFIG_OPTION_PIN_MODE_INPUT_BUFFER_ON_NO_RESISTORS);
+
+	//Set up UART
+	uart_configure_8_n_1_9600();
+}
+
+void _sleep(uint8_t s) 
+{
+	do {	
+		// sleep, wake by rtc2 tick
+		pwr_clk_mgmt_enter_pwr_mode_register_ret();
+		pwr_clk_mgmt_enter_pwr_mode_standby();
+	} while (--s);
+}
+
 void setup()
 {
-	msg.msgtype = MSG_POWER_METER;
+	msg.msgtype = MSG_COUNT_VOLTS;
 	msg.voltage = 5.0001;
-	msg.current = 1.0001;
-	msg.true_power = 100.01;
-	msg.power_factor = 0.99;
 
-	serialBegin();
+	uart_init();
 
-	printf("nrf24le1 node starting..\r\n");
+	printf("nrf24le1 node starting..\n");
 	//watchdogRun(1000);
 	
 	pinMode(P0_0, INPUT | PULLUP);
@@ -63,42 +101,16 @@ void setup()
 	digitalWrite(LED3, LED_OFF);
 	/*
 	rf_configure_debug_lite(false, 32);	
-
-	rf_configure( RF_CONFIG_DEFAULT_VAL | RF_CONFIG_PWR_UP,	// uint8_t config
-				 false,							// bool opt_rx_active_mode
-				 RF_EN_AA_ENAA_NONE,			// uint8_t en_aa
-				 RF_EN_RXADDR_DEFAULT_VAL,		// uint8_t en_rxaddr
-				 RF_SETUP_AW_DEFAULT_VAL,		// uint8_t setup_aw
-				 RF_SETUP_RETR_DEFAULT_VAL,		// uint8_t setup_retr
-				 0x01,							// uint8_t rf_ch
-				 RF_RF_SETUP_DEFAULT_VAL,		// uint8_t rf_setup
-				 NULL,							// uint8_t * rx_addr_p0
-				 NULL,							// uint8_t * rx_addr_p1
-				 RF_RX_ADDR_P2_DEFAULT_VAL,		// uint8_t rx_addr_p2
-				 RF_RX_ADDR_P3_DEFAULT_VAL,		// uint8_t rx_addr_p3
-				 RF_RX_ADDR_P4_DEFAULT_VAL,		// uint8_t rx_addr_p4
-				 RF_RX_ADDR_P5_DEFAULT_VAL,		// uint8_t rx_addr_p5
-				 txaddr,							// uint8_t * tx_addr
-				 RF_RX_PW_P0_DEFAULT_VAL,		// uint8_t rx_pw_p0
-				 RF_RX_PW_P1_DEFAULT_VAL,		// uint8_t rx_pw_p1
-				 RF_RX_PW_P2_DEFAULT_VAL,		// uint8_t rx_pw_p2
-				 RF_RX_PW_P3_DEFAULT_VAL,		// uint8_t rx_pw_p3
-				 RF_RX_PW_P4_DEFAULT_VAL,		// uint8_t rx_pw_p4
-				 RF_RX_PW_P5_DEFAULT_VAL,		// uint8_t rx_pw_p5
-				 RF_DYNPD_DPL_NONE,				// uint8_t dynpd
-				 RF_FEATURE_NONE);				// uint8_t feature
 	*/
-	//rf_configure_debug(false, 4, false);
-
 	rf_configure( RF_CONFIG_EN_CRC | RF_CONFIG_CRCO | RF_CONFIG_PWR_UP,	// uint8_t config
 				 false,							// bool opt_rx_active_mode
-				 RF_EN_AA_ENAA_NONE,			// uint8_t en_aa
+				 RF_EN_AA_ENAA_ALL,			// uint8_t en_aa
 				 RF_EN_RXADDR_DEFAULT_VAL,		// uint8_t en_rxaddr
 				 RF_SETUP_AW_5BYTES,		// uint8_t setup_aw
-				 RF_SETUP_RETR_DEFAULT_VAL,		// uint8_t setup_retr
-				 0,							// uint8_t rf_ch
+				 RF_SETUP_RETR_ARD_1000 | RF_SETUP_RETR_ARC_4,		// uint8_t setup_retr
+				 1,							// uint8_t rf_ch
 				 RF_RF_SETUP_RF_DR_250_KBPS | RF_RF_SETUP_RF_PWR_NEG_12_DBM,		// uint8_t rf_setup
-				 NULL,							// uint8_t * rx_addr_p0
+				 txaddr,							// uint8_t * rx_addr_p0
 				 NULL,							// uint8_t * rx_addr_p1
 				 RF_RX_ADDR_P2_DEFAULT_VAL,		// uint8_t rx_addr_p2
 				 RF_RX_ADDR_P3_DEFAULT_VAL,		// uint8_t rx_addr_p3
@@ -116,24 +128,24 @@ void setup()
 
 	rf_set_as_tx();
 
+	// rtc stuff
+	pwr_clk_mgmt_clklf_configure(PWR_CLK_MGMT_CLKLF_CONFIG_OPTION_CLK_SRC_RCOSC32K);
+	rtc2_configure(RTC2_CONFIG_OPTION_ENABLE | 
+			RTC2_CONFIG_OPTION_COMPARE_MODE_0_RESET_AT_IRQ |
+			RTC2_CONFIG_OPTION_DO_NOT_CAPTURE_ON_RFIRQ,
+			0x7FFF); //rtc tick = 1 sec
+	//rtc2_run();
+	interrupt_control_rtc2_enable();
+	sti();
 	// adc setup
-
 }
 
 void loop()
 {
-	//debugPrint("%s\n", mystring);
+	watchdog_set_wdsv_count(WATCHDOG_TIME); // reset wdt
 	digitalWrite(LED2, LED_ON);	
-	delay(100);
+	delay(20);
 	digitalWrite(LED2, LED_OFF);	
-	delay(500);
-	digitalWrite(LED3, LED_ON);
-	delay(200);
-	digitalWrite(LED3, LED_OFF);	
-	delay(500);
-	
-	regbuf[0] = counter & 0xFF;
-	regbuf[1] = (counter >> 8) & 0xFF;
 
 	adc_configure( ADC_CONFIG_OPTION_ACQ_TIME_3_US | 
 		ADC_CONFIG_OPTION_REF_SELECT_INT_1_2V | 
@@ -144,14 +156,31 @@ void loop()
 	msg.counter++;
 	msg.voltage = raw2volts(adcraw);
 
-	rf_write_tx_payload_noack((uint8_t *) msg, sizeof(msg), true);
-	//rf_transmit();
+	// rf stuff
+	rf_flush_tx();
+	rf_irq_clear_all();
 
-	while(!(rf_irq_pin_active() && rf_irq_tx_ds_active()));
-	//rf_irq_clear_all();
+	rf_write_tx_payload((uint8_t *) msg, sizeof(msg), true);
 
-	printf("sent %d. adcraw: %d, voltage: %0.3f\r\n", counter, adcraw, msg.voltage);
-	delay(200);
+	// wait for rf transactino to complete
+	while (!(rf_irq_pin_active()));
+	if (rf_irq_tx_ds_active()) {
+		// tx success
+	} else if (rf_irq_max_rt_active()) {
+		// tx retries failed
+	}
+
+	//while(!(rf_irq_pin_active() && rf_irq_tx_ds_active()));
+	//while (rf_irq_max_rt_active()); // trap retrans timeout to trigger wdt
+
+	printf("t: %4ld, cnt: %4d, adcraw: %4d, voltage: %0.3f\r\n", seconds, counter, adcraw, msg.voltage);
+
+	digitalWrite(LED3, LED_ON);
+	delay(20);
+	digitalWrite(LED3, LED_OFF);	
 	counter++;
+	//delay(21000);
+	// power save
+	_sleep(8);
 
 }
